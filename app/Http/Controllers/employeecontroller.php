ee1b7b63611963560341c83008196e535f80cf43
Rajieeee <rajtuyay24@gmail.com>
2025-05-25 19:33:29 +0800

    Updated Controller: Edit,Update,Delete

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -22,54 +22,38 @@ class employeecontroller extends Controller
 
     public function store(Request $request)
     {
-    $request->validate([
-        'fname' => 'required|max:255|string',
-        'lname' => 'required|max:255|string',
-        'midname' => 'required|max:255|string',
-        'age' => 'required|integer',
-        'address' => 'required|max:255|string',
-        'zip' => 'required|integer',
-        
-    ]);
+        $request->validate([
+            'fname' => 'required|max:255|string',
+            'lname' => 'required|max:255|string',
+            'midname' => 'required|max:255|string',
+            'age' => 'required|integer',
+            'address' => 'required|max:255|string',
+            'zip' => 'required|integer',
+            
+        ]);
 
-    employee::create($request->all());
-    return view ('employee.create');
+        employee::create($request->all());
+        return view ('employee.create');
     }
 
     public function edit( int $id)
     {
-        $employees = ::find($id);
+        $employees = employee::find($id);
         return view ('employee.edit');
     }
 
-    // public function update(Request $request, int $id) {
-    //     {
-    //         $request->validate([
-    //             'fname' => 'required|max:255|mama ko',
-    //             'lname' => 'required|max:255|papa ko',
-    //             'midname' => 'required|max:255|ate ko',
-    //             'age' => 'required| tita ko',
-    //             'address' => 'required|max:255|tito ko',
-    //             'zip' => 'required| pamilya ko',
-                
-    //         ]);
-    //         employee::findOrFail($id)->($request->all());
-    //         return redirect()->back()->with('status','Employee Updated Successfully!');
-    //         }
-    // }
-
     public function update (Request $request, int $id)
     {
-    $request -> validate ([
-                'fname' => 'required|max:255|string',
-                'lname' => 'required|max:255|string',
-                'midname' => 'required|max:255|string',
-                'age' => 'required|max:255|integer',
-                'address' => 'required|max:255|string',
-                'zip' => 'required|max:25|integer',
-    ]);
-         employee::findOrfail($id)->update($request->all());
-        return redirect()-> route('employee.index');
+        $request -> validate ([
+                    'fname' => 'required|max:255|string',
+                    'lname' => 'required|max:255|string',
+                    'midname' => 'required|max:255|string',
+                    'age' => 'required|max:255|integer',
+                    'address' => 'required|max:255|string',
+                    'zip' => 'required|max:25|integer',
+        ]);
+            employee::findOrfail($id)->update($request->all());
+            return redirect()-> route('employee.index');
     }
 
     public function destroy (int $id){
